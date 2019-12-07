2afef2ab04d797e41aca1741cb612fe643bc5b33
刘强 <784248173@qq.com>
2019-12-07 13:45:49 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=784248173@qq.com
 
 export MYCLUSTER=tektonknativebeijing31
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing31/kube-config-tok02-tektonknativebeijing31.yml
 
 export NAMESPACE=liuqiang
 export EMAIL=784248173@qq.com
